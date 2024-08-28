db1a5f6b0fbc4c24095122fa3b6301e46d7e2c25
Marisol <marisol.azofeifa@totemtech.net>
2024-08-28 08:31:09 -0600

    scale

--- app/components/CaseProject.js
+++ app/components/CaseProject.js
@@ -1,8 +1,13 @@
-import React from 'react'
+'use client'
+import { useTransform, motion } from "framer-motion";
 
-export default function CaseProject() {
+export default function CaseProject({progress}) {
+  const scaleY = useTransform(progress, [0, 1], [0, 1]);
   return (
-    <div className=' bg-slate-400 min-h-[400px]  lg:min-h-[700px]  w-full flex justify-center items-center' >
+    <div className=' flex-col bg-slate-400 min-h-[400px]  lg:min-h-[700px]  w-full flex justify-center items-center ' >
+      <motion.div style={{ scaleY }}  className=' w-full h-full bg-gray-900 origin-bottom' >
+
+      </motion.div>
       Proyecto
     </div>
   )
--- app/components/Cases.js
+++ app/components/Cases.js
@@ -1,14 +1,26 @@
 import CaseProject from "./CaseProject"
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  MotionValue
+} from "framer-motion";
 
 export default function Cases() {
+  const container = useRef();
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"]
+  }) 
   return (
-    <section className="bg-gray-900  w-full min-h-48 z-10" >
+    <section ref={container} className="bg-gray-900  w-full min-h-48 z-10" >
         <div className=" bg-black/5 h-full w-full flex items-center justify-center py-28">
         <div className="max-w-[2000px] w-full px-5 md:px-10 gap-[40px] lg:gap-[60px] flex flex-col lg:flex-row ">
-
-            <CaseProject />
-            <CaseProject />
-            <CaseProject />
+            <CaseProject progress={scrollYProgress} />
+            <CaseProject progress={scrollYProgress} />
+            <CaseProject progress={scrollYProgress} />
         </div>
         </div>
     </section>
