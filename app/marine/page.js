78cdebc65a7668cb539469185bc8e8008816179a
Marisol <marisol.azofeifa@totemtech.net>
2024-09-19 16:42:24 -0600

    Team

--- app/marine/page.js
+++ app/marine/page.js
@@ -5,7 +5,7 @@ import HeroCarrusel from "@/app/components/marine/HeroCarrusel";
 import Header from "@/app/components/marine/Header";
 import WhyUsTitle from "../components/marine/WhyUsTitle";
 import WhyUsSection from "../components/marine/WhyUsSection";
-import CardServices from "../components/marine/CardServices";
+import Team from "../components/Team";
 
 
 import { useEffect } from "react";
@@ -35,6 +35,10 @@ export default function Home() {
         <HeroCarrusel />
         <WhyUsTitle />
         <WhyUsSection />
+        <div className=" bg-marine-gray w-full flex justify-center">
+
+        <Team />
+        </div>
       
       <section className="  bg-red-700 w-full h-svh z-10"></section>
       <section className="  bg-black w-full h-svh z-10"></section>
